9ab3260790d603362e0f168212aaf94a57212c43
Андрій Пікусь <llityhga@gmail.com>
2024-06-25 15:57:09 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Wolf on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
